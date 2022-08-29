33e6e76ae1e1961b4141f12ad610b67b2903ab69
Mikael <mikaelangelopizzi@gmail.com>
2022-08-29 19:14:46 -0400

    Create Save and Open buttons, improve logic for base case

--- editor.php
+++ editor.php
@@ -3,8 +3,13 @@
 // Declare a variable that represents the file
 $file = 'test file.txt';
 
-// Method to modify the file via GET request
-file_put_contents($file, $_GET['text']);
+if(isset($_GET['file'])) { // If file input text is not empty, get the content
+    $file = $_GET['file'];
+    
+    if ($_GET['action'] == 'Save') {  // If you click 'Save' button, save the file
+        file_put_contents($file, $_GET['text']); // Method to modify the file via GET request
+    }
+}
 
 // Method to get the file content
 $text = file_get_contents($file);
@@ -13,11 +18,12 @@ $text = file_get_contents($file);
 
 <h1>Text editor</h1>
 
-<br />
 <form>
-<textarea name="text"><?= $text ?></textarea>
-<br />
-<input type="submit" value="Save" />
+    <label>File Name:</label>
+    <input type="text" name="file" value="<?= $file ?>" /><br /><br />
+    <textarea name="text"><?= $text ?></textarea><br />
+    <input type="submit" value="Save" name="action" />
+    <input type="submit" value="Open" name="action" />
 </form>
 
 <style>
